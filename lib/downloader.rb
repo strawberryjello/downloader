82730e3814fe67696b67935f379eba947bb3d237
Tina Albores <maalbores@gmail.com>
2019-01-06 12:09:12 +0800

    started using the URI class (stdlib) for URl parsing and manipulation

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -1,25 +1,42 @@
 require "downloader/version"
 require "downloader/errors"
 require 'http'
+require 'uri'
 
 module Downloader
+  # TODO: move to utility class in separate file
+  def self.extract_domain(url)
+    uri = URI(url)
+    host = uri.host
+    scheme = uri.scheme
+
+    raise UriError, "Missing scheme" unless scheme
+
+    case (scheme)
+    when 'https'
+      URI::HTTPS.build(host: host)
+    when 'http'
+      URI::HTTP.build(host: host)
+    else
+      raise UriError, "Scheme not implemented: #{scheme}"
+    end
+  end
 
   def self.batch(input_file, dest)
+    # TODO: add file error handling (new error class)
     urls = File.open(input_file, 'r').readlines
 
-    # this could be made more generic,
-    # eg for domains that don't end in .com
-    domain = urls[0].slice(/http[s]?:\/\/[a-z0-9\.]+.com/)
+    domain = extract_domain(urls[0])
 
     puts domain
 
     http = HTTP.persistent(domain)
 
     urls.each do |url|
-      partial_url = url.sub(domain, '')
+      partial_url = URI(url).path
       puts partial_url
 
-      filename = url.slice(/\/[a-zA-Z0-9_.\-]+$/).chomp
+      filename = partial_url.slice(/\/[a-zA-Z0-9_.\-]+$/).chomp
       File.open(File.join(dest, filename), 'w') do |f|
         f.write(http.get(partial_url))
       end
