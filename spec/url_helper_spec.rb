d576a7f41c2231691c6e09c08798799bd066f787
Tina Albores <maalbores@gmail.com>
2019-01-06 18:48:43 +0800

    added tests for UrlHelper methods for extracting filenames and relative refs from URLs

--- spec/url_helper_spec.rb
+++ spec/url_helper_spec.rb
@@ -41,4 +41,36 @@ RSpec.describe Downloader::UrlHelper do
       expect { Downloader::UrlHelper.extract_host_with_scheme("irc://www.example.com") }.to raise_error(Downloader::UriError, 'Cannot handle scheme: irc')
     end
   end
+
+  describe '#extract_filename' do
+    it 'extracts a filename with an extension at the end of the URL path' do
+      expect(Downloader::UrlHelper.extract_filename('https://www.example.com/cats/catting.jpg')).to eq('catting.jpg')
+    end
+
+    it 'extracts a filename without an extension at the end of the URL path' do
+      expect(Downloader::UrlHelper.extract_filename('https://www.example.com/cats/2cat')).to eq('2cat')
+    end
+
+    it 'raises a UriError when a filename cannot be extracted' do
+      expect { Downloader::UrlHelper.extract_filename('https://www.example.com/cats/') }.to raise_error(Downloader::UriError, 'Cannot extract filename from path: /cats/')
+    end
+  end
+
+  describe '#extract_relative_ref' do
+    it 'extracts a relative reference from a URL with a path' do
+      expect(Downloader::UrlHelper.extract_relative_ref('https://www.example.com/cats')).to eq(URI('/cats'))
+    end
+
+    it 'extracts a relative reference from a URL with a path and a query string' do
+      expect(Downloader::UrlHelper.extract_relative_ref('https://www.example.com/cats?id=1&tails=2')).to eq(URI('/cats?id=1&tails=2'))
+    end
+
+    it 'extracts a relative reference from a URL with a path, a query string, and a fragment' do
+      expect(Downloader::UrlHelper.extract_relative_ref('https://www.example.com/cats?id=1&tails=2#bottom')).to eq(URI('/cats?id=1&tails=2#bottom'))
+    end
+
+    it 'raises a UriError when a path cannot be extracted' do
+      expect { Downloader::UrlHelper.extract_relative_ref('https://www.example.com') }.to raise_error(Downloader::UriError, 'Cannot extract path from URL: https://www.example.com')
+    end
+  end
 end
