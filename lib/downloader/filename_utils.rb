e782a2c03e6ca5fb6ee5b2f222d893bf25ad637f
Tina Albores <maalbores@gmail.com>
2019-01-12 21:27:37 +0800

    used FilenameError for input checking in FilenameUtils.rename_to_number, added tests

--- lib/downloader/filename_utils.rb
+++ lib/downloader/filename_utils.rb
@@ -2,6 +2,9 @@ module Downloader
   # TODO: consider merging this with the FilenameUtils in imgd and extracting to a gem
   class FilenameUtils
     def self.rename_to_number(filename, number)
+      raise FilenameError, "Missing filename" unless filename
+      raise FilenameError, "Missing file number" unless number
+
       extension = File.extname(filename)
 
       "#{number}#{extension}"
--- spec/filename_utils_spec.rb
+++ spec/filename_utils_spec.rb
@@ -1,5 +1,6 @@
 require "downloader"
 require "downloader/filename_utils"
+require "downloader/errors"
 
 RSpec.describe Downloader::FilenameUtils do
   describe '#rename_to_number' do
@@ -9,6 +10,18 @@ RSpec.describe Downloader::FilenameUtils do
 
     it 'returns a filename containing only a number if there was no file extension' do
       expect(Downloader::FilenameUtils.rename_to_number("cat", 2)).to eq("2")
+    end
+
+    it 'raises a FilenameError when number is nil' do
+      expect { Downloader::FilenameUtils.rename_to_number("cat.jpg", nil) }.to raise_error(Downloader::FilenameError, "Missing file number")
+    end
+
+    it 'raises a FilenameError when filename is nil' do
+      expect { Downloader::FilenameUtils.rename_to_number(nil, 1) }.to raise_error(Downloader::FilenameError, "Missing filename")
+    end
+
+    it 'raises a FilenameError when filename and number are nil' do
+      expect { Downloader::FilenameUtils.rename_to_number(nil, nil) }.to raise_error(Downloader::FilenameError, "Missing filename")
     end
   end
 end
